a00b191307de55a3732a8efb05669ba327bb7b07
Delia Kossen <delia.kossen@students.fhnw.ch>
2022-04-07 15:48:50 +0200

    [change] 'security' steps 1-4

---
+++ ex07-contactlist-sec/src/main/java/webeng/contactlist/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package webeng.contactlist;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/*.css").permitAll()
+                    .antMatchers("/contacts").authenticated()
+                    .regexMatchers("/contacts/[0-9]+").authenticated()
+                    .anyRequest().hasRole("ADMIN")
+                    .and()
+                .formLogin();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        var user = User.withDefaultPasswordEncoder()
+                .username("user").password("user")
+                .roles().build();
+        var admin = User.withDefaultPasswordEncoder()
+                .username("admin").password("admin")
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+}
